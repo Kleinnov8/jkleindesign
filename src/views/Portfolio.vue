e4f2ab96386eafee95574aaa96cfe4eac811a04e
Optimus <kleinnov8@gmail.com>
2025-12-29 02:26:41 -0600

    Add under construction text to Portfolio.vue and Services.vue

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,5 +1,6 @@
 <template>
     <section class="glass-container">
         <h2>My Work</h2>
+        <h3>UNDER CONSTRUCTION</h3>
     </section>
 </template>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,5 +1,6 @@
 <template>
     <section class="glass-container">
         <h2>Services</h2>
+        <h3>UNDER CONSTRUCTION</h3>
     </section>
 </template>
