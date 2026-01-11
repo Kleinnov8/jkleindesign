aa746e86fe17f095881ef711116a80252bd3a6b0
Optimus <kleinnov8@gmail.com>
2026-01-10 22:55:27 -0600

    Create portfolio.js to house the data for portfolio projects

---
+++ src/data/portfolio.js
@@ -0,0 +1,35 @@
+// This is an array of objects. Each object is one project.
+export const projects = [
+    {
+        id: 1,
+        title: "Oilfield Tech Origins",
+        category: "Web Design and Document Conversion",
+        image: "",
+        description: "A responsive blog website based on a published e-book, focusing on the origins of oilfield technology.",
+        link: "/case-study/oilfield-tech-origins"
+    },
+    {
+        id: 2,
+        title: "Camp Bow Wow - Chairman's Ball Poster (Judge's Choice Award)",
+        category: "Print and Video",
+        image: "",
+        description: "A custom poster inspired by old-school circus posters, and the video that played on a screen in the middle of the poster.",
+        link: "/case-study/cbw-chairmans-ball-poster"
+    },
+    {
+        id: 3,
+        title: "Camp Bow Wow - Print and Branding",
+        category: "Print and Branding",
+        image: "",
+        description: "Full suite of print marketing materials and brand collateral.",
+        link: "#"
+    },
+    {
+        id: 4,
+        title: "Farley's Fit Kitchen",
+        category: "Web Design and Branding",
+        image: "",
+        description: "A responsive meal-prep website focusing on mobile conversion.",
+        link: "/case-study/farleys-fit-kitchen"
+    },
+]
