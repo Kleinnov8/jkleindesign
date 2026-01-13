2e2bcde6a24414c927c204c090d6d5ca3bb80786
Optimus <kleinnov8@gmail.com>
2026-01-12 21:41:13 -0600

    casestudy.vue initial commit

---
+++ src/views/CaseStudy.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="case-study-container">
+        <div class="placeholder-content">
+            <h1>Project Case Study</h1>
+            <p>Currently viewing project ID: <strong>{{ $route.params.id }}</strong></p>
+            <router-link to="/portfolio" class="back-link">← Back to Portfolio</router-link>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    
+</script>
