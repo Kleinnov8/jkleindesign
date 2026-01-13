91d47c9b4ae46e3ac72c142756a722a6cf66a070
Optimus <kleinnov8@gmail.com>
2026-01-12 22:10:21 -0600

    add routing for case studies

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Portfolio from '../views/Portfolio.vue'
 import Services from '../views/Services.vue'
+import CaseStudy from '../views/CaseStudy.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/portfolio', component: Portfolio },
-    { path: '/services', component: Services }
+    { path: '/services', component: Services },
+    { path: '/case-study/:id', name: 'CaseStudy', component: CaseStudy, props: true }
 ]
 
 const router = createRouter({
